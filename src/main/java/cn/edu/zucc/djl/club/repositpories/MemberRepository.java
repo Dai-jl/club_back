40a607c0c62891310b7cfa010c033e3a12eb895c
Dai-jl <Dai_jl@outlook.com>
2020-05-24 16:02:14 +0800

    test

--- src/main/java/cn/edu/zucc/djl/club/repositpories/MemberRepository.java
+++ src/main/java/cn/edu/zucc/djl/club/repositpories/MemberRepository.java
@@ -1,10 +1,51 @@
+
+
 package cn.edu.zucc.djl.club.repositpories;
 
 import cn.edu.zucc.djl.club.entity.MemberTableEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<MemberTableEntity,Integer> {
-    List<MemberTableEntity> findBycIdOrderByStateDesc(int cid);
+    //通过社团id查找社团成员
+//    @Transactional
+//    @Query(value = "select * from member_table where c_id=?1 limit ?2,10",nativeQuery = true)
+//    @Modifying
+//    List<MemberTableEntity> getMemList(int cid,int startIndex);
+    MemberTableEntity findByUId(String id);
+
+    //通过社团id查找社团成员的全部信息
+    @Transactional
+    @Query(value = "select s.u_name,s.phone,c.c_name,mt.join_date,mt.leave_date,mt.state,s.u_id" +
+            " from member_table mt,student s,college c where mt.c_id=?1 and mt.u_id=s.u_id and s.college_id=c.c_id LIMIT ?2,10",nativeQuery = true)
+    List<Object[]> getMemList(int cid, int startIndex);
+
+    //转让社长
+    //修改社长身份
+    @Transactional
+    @Query(value = "update member_table set type='member',leave_date=now() where u_id=?1 and c_id=?2",nativeQuery = true)
+    @Modifying
+    int transferMsgLeader(String uid,int cid);
+
+    //修改成员身份
+    @Transactional
+    @Query(value = "update  member_table set type='leader' where u_id=?1 and c_id=?2",nativeQuery = true)
+    @Modifying
+    int transferMsgMem(String uid,int cid);
+
+    //判断成员是否存在
+    boolean existsByCIdAndUId(int cid,String uid);
+
+    //删除成员
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE  member_table SET state=0 ,leave_date = NOW() WHERE c_id = ? AND u_id = ?", nativeQuery = true)
+    void updatestate(int cid,String uid);
+
+
 }
+
